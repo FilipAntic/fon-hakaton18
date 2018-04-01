79ef043fd8a744cd3a04f8dd9bff1baabd3b6aae
MazZa <m4zz4@live.com>
2018-04-01 02:55:50 +0200

    Sve sam uradio

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,9 @@ import { TabViewModule } from 'primeng/tabview';
 import {DropdownModule} from 'primeng/dropdown';
 import {InputTextModule} from 'primeng/inputtext';
 import {ButtonModule} from 'primeng/button';
+import { DataTableModule } from 'primeng/datatable';
+import {TableModule} from 'primeng/table';
+
 
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
@@ -37,7 +40,9 @@ import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
     FormsModule,
     BrowserAnimationsModule,
     InputTextModule,
-    ButtonModule
+    ButtonModule,
+    DataTableModule,
+    TableModule
   ],
   providers: [AuthService],
   bootstrap: [AppComponent]
--- src/app/category-search/category-search.component.ts
+++ src/app/category-search/category-search.component.ts
@@ -1,8 +1,13 @@
-import { Component, OnInit } from '@angular/core';
+import {AfterViewInit, ChangeDetectorRef, Component, OnInit,} from '@angular/core';
+import { Chart } from 'chart.js';
 
 interface Company {
   name: string;
   phoneNumber: string;
+  pocetakUgovora: string;
+  istekUgovora: string;
+  brKorisnika: string;
+  brAkcija: string;
 }
 @Component({
   selector: 'app-category-search',
@@ -13,23 +18,69 @@ interface Company {
 
 
 
-export class CategorySearchComponent implements OnInit {
+export class CategorySearchComponent implements OnInit, AfterViewInit {
 
-
+  chart = [];
+  imena: string[] = ['jedan', 'dva', 'tri'];
 
   companies: Company[];
 
   selectedCompany: Company;
 
-  constructor() {
+  constructor(private cdRef: ChangeDetectorRef) {
 
     this.companies = [
-      {name: 'BigPizza', phoneNumber: '066/441-441'},
-      {name: 'BeoTaxi', phoneNumber: '065888888'}
+      {name: 'BigPizza',
+        phoneNumber: '066/441-441',
+        pocetakUgovora: '25.03.2018',
+        istekUgovora: '25.03.2019',
+        brKorisnika: '12589',
+        brAkcija: '3691'},
+      {name: 'BeoTaxi',
+        phoneNumber: '065888888',
+        pocetakUgovora: '25.03.2018',
+        istekUgovora: '25.03.2019',
+        brKorisnika: '12589',
+        brAkcija: '3691'}
     ];
+    this.selectedCompany = this.companies[0];
   }
 
   ngOnInit() {
+  }
+
+  ngAfterViewInit() {
+    this.chart = new Chart('canvas', {
+      type: 'line',
+
+      // The data for our dataset
+      data: {
+        labels: ['1', '2', '3', '4', '5', '6', '7'],
+        datasets: [{
+          label: 'Broj poziva prema BigPici',
+          data: [500, 1000, 700, 850, 100, 500, 1500],
+          backgroundColor: [
+            'rgb(255, 50, 25)',
+            ],
+          borderColor: [
+            'rgb(255, 50, 25)',
+          ],
+          fill: false,
+        }]
+      },
+
+      // Configuration options go here
+      options: {
+        scales: {
+          yAxes: [{
+            ticks: {
+              beginAtZero: true
+            }
+          }]
+        }
+      }
+    });
+    this.cdRef.detectChanges();
   }
 
 }
